7e921321b19f48e44f0a962b8f248c07ffcd89bb
Bill Cui <bill.cui.work@gmail.com>
2022-08-02 22:53:20 -0400

    refactor and inx

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn main() {
 mod test {
     use super::*;
     #[test]
-    fn test_0xa9_lda_immidiate_load_data() {
+    fn test_0xa9_lda_immediate_load_data() {
         let mut cpu = CPU::new();
         cpu.interpret(vec![0xa9, 0x05, 0x00]);
         assert_eq!(cpu.register_a, 0x05);
@@ -21,6 +21,32 @@ mod test {
         assert!(cpu.status & PS_ZERO_MASK != 0);
         assert!(cpu.status & PS_NEGATIVE_MASK == 0);
     }
+
+    #[test]
+    fn test_0xaa_tax_move_a_to_x() {
+        let mut cpu = CPU::new();
+        cpu.register_a = 10;
+        cpu.interpret(vec![0xaa, 0x00]);
+
+        assert_eq!(cpu.register_x, 10)
+    }
+
+    #[test]
+    fn test_5_ops_working_together() {
+        let mut cpu = CPU::new();
+        cpu.interpret(vec![0xa9, 0xc0, 0xaa, 0xe8, 0x00]);
+
+        assert_eq!(cpu.register_x, 0xc1)
+    }
+
+    #[test]
+    fn test_inx_overflow() {
+        let mut cpu = CPU::new();
+        cpu.register_x = 0xff;
+        cpu.interpret(vec![0xe8, 0xe8, 0x00]);
+
+        assert_eq!(cpu.register_x, 1)
+    }
 }
 
 const PS_CARRY_MASK: u8 = 0b0000_0001;
@@ -32,6 +58,7 @@ const PS_NEGATIVE_MASK: u8 = 0b1000_0000;
 
 pub struct CPU {
     pub register_a: u8,
+    pub register_x: u8,
     pub status: u8,
     pub program_counter: u16,
 }
@@ -40,9 +67,39 @@ impl CPU {
     pub fn new() -> Self {
         CPU {
             register_a: 0,
+            register_x: 0,
             status: 0,
             program_counter: 0,
         }
+    }
+
+    fn update_zero_and_negative_flags(&mut self, result: u8) {
+        if result == 0 {
+            self.status = self.status | PS_ZERO_MASK;
+        } else {
+            self.status = self.status & !PS_ZERO_MASK;
+        }
+
+        if (result as i8) < 0 {
+            self.status = self.status | PS_NEGATIVE_MASK;
+        } else {
+            self.status = self.status & !PS_NEGATIVE_MASK;
+        }
+    }
+
+    fn lda(&mut self, value: u8) {
+        self.register_a = value;
+        self.update_zero_and_negative_flags(self.register_a);
+    }
+
+    fn tax(&mut self) {
+        self.register_x = self.register_a;
+        self.update_zero_and_negative_flags(self.register_x);
+    }
+
+    fn inx(&mut self) {
+        self.register_x = self.register_x.wrapping_add(1);
+        self.update_zero_and_negative_flags(self.register_x);
     }
 
     pub fn interpret(&mut self, program: Vec<u8>) {
@@ -56,22 +113,16 @@ impl CPU {
                 0xA9 => {
                     let param = program[self.program_counter as usize];
                     self.program_counter += 1;
-                    self.register_a = param;
-
-                    if self.register_a == 0 {
-                        self.status = self.status | PS_ZERO_MASK;
-                    } else {
-                        self.status = self.status & !PS_ZERO_MASK;
-                    }
-
-                    if (self.register_a as i8) < 0 {
-                        self.status = self.status | PS_NEGATIVE_MASK;
-                    } else {
-                        self.status = self.status & !PS_NEGATIVE_MASK;
-                    }
+                    self.lda(param);
                 }
                 0x000 => {
                     return;
+                }
+                0xAA => {
+                    self.tax();
+                }
+                0xE8 => {
+                    self.inx();
                 }
                 _ => todo!(),
             }
